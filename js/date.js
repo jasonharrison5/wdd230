c6d283621d6290436bfdad15140db70013b1e5f6
jasonharrison5 <har18130@byui.edu>
2023-01-21 23:51:18 -0700

    Chamber of commerce

--- js/date.js
+++ js/date.js
@@ -1,32 +1,7 @@
-let daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
+// Select the HTML element to manipulate
+const date = new Date(document.lastModified);
+const options = {weekday :'numeric', day: 'numeric', month: 'numeric', year: 'numeric'}
+// document.getElementById('lastmod').textContent = new Date().toDateString('en-US', options);
 
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-let d = new Date();
-let dayName = dayname[d.getDay()];
-let monthName = months[d.getMonth()];
-let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " +d.getFullYear();
-document.getElementById("currentdate").testContent = fulldate;
-try {
-    let options = 
-}
+let mod = document.lastModified;
+document.getElementById("lastmod").textContent = mod; 
